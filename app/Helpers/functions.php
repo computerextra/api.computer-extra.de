bb01c561b37b80e79e1143fe31d4128d3f070a99
jayzone91 <register@j4yz0n3.de>
2025-08-20 12:56:13 +0200

    angefangen

---
+++ app/Helpers/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Constants\GenrealConstant;
+
+function env(string $key, string $default = null)
+{
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
+    $dotenv->load();
+    return $_ENV[$key] ?? $default;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/soheilkhaledabdi/php-crud-api-flight/blob/main/app/Constants/GeneralConstant.php
