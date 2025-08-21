0af7da9577bef2eb0bf8401d222c78a458e00256
jayzone91 <register@j4yz0n3.de>
2025-08-21 12:20:11 +0200

    Update abteilungen.php

--- abteilungen.php
+++ abteilungen.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 header('Access-Control-Allow-Origin: *');
 header("Content-Type:applicytion/json");
 require_once "./config.php";
