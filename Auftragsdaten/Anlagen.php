98d6ec834d087da3c1f77e831288db3971205a0a
jayzone91 <register@j4yz0n3.de>
2025-09-02 12:41:54 +0200

    Update Anlagen.php

--- Auftragsdaten/Anlagen.php
+++ Auftragsdaten/Anlagen.php
@@ -36,31 +36,31 @@ function A_1(): string
         umfasst folgende Arbeiten und/oder Leistungen:
     </p>
     <ul class="check" style="list-style-type: none !important;">
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Auftragsverarbeitung in unserem Warenwirtschaftssystem und Intranet
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Durchführung von Fernwartungen
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Elektronische Datenverarbeitung zur Bestellabwicklung sowie Warenlieferung
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Support Tätigkeiten bei Problemen und Störungen
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Verwaltung von Securepoint Firewalls und deren Lizenzen
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Durchführung von Datensicherungen und Datenvernichtungen
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Versand von Waren mittels Versanddienstleister
         </li>
@@ -90,32 +90,32 @@ function A_2(): string
         Verarbeitung personenbezogener Daten unter Einsatz von Datenverarbeitungsanlagen:
     </p>
     <ul class="check" style="list-style-type: none !important;">
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Verändern (z.B. durch Änderungen der Benutzer an den Datensätzen)
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Übermitteln (z.B. durch Übermittlung per E-Mail von Nachrichten)
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Speichern (z.B. durch Sicherung und Archivierung auf Festplatten, anderen
             Speichersystemen oder Datenträgern)
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Löschen (z.B. durch Löschen von Datensätzen oder Vernichten von Datenträgern)
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Erheben (z.B. durch Einlesen von Mitarbeiterlisten und anderen Daten)
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Einschränken (z.B. durch Deaktivierung von einzelnen Datensätzen)
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Nutzen (z.B. durch Durchführung von Auswertungen)
         </li>
@@ -129,11 +129,11 @@ function A_3(): string
     $body = <<<HTML
     <h2>3. Art der Daten</h2>
     <ul class="check" style="list-style-type: none !important;">
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
            Beschäftigtendaten (z.B. Name, Vorname, Benutzername, E-Mailadresse).
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Nutzungsdaten (z.B. Protokollierung der Nutzeraktivitäten, Log-Files nur mit anonymer IP-Adresse).
         </li>
@@ -147,15 +147,15 @@ function A_4(): string
     $body = <<<HTML
     <h2>4. Kategorien der betroffenen Personen</h2>
     <ul class="check" style="list-style-type: none !important;">
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
            Beschäftigte des Auftraggebers
         </li>
-        <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+        <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Beauftragte des Auftraggebers
         </li>
-          <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle;">
+          <li style="display: flex; gap: 0.5rem; align-items: baseline;">
             <input type="checkbox" checked disabled />
             Lieferanten
         </li>
@@ -170,118 +170,118 @@ function A_5(): string
     <h2>5. Eingesetzte Subunternehmer</h2>
     <p>Folgende Subunternehmer werden von uns eingesetzt.</p>
     <ul class="check" style="list-style-type: none !important">
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Securepoint GmbH, Bleckeder Landstraße 28, 21337 Lüneburg ((Fern-) Prüfung
         und Wartung Securepoint)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Meinhardt Städtereinigung GmbH & Co. KG, Nassaustraße 13-15, 65719
         Hofheim-Wallau (Aktenvernichtung)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         audatis Services GmbH, Luisenstraße 1, 32052 Herford (Datenschutzmanager)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Terra Cloud GmbH, Hankamp 2, 32609 Hüllhorst (Cloud Computing)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Brother International GmbH, Konrad-Adenauer-Allee 1-11, 61118 Bad Vilbel
         (Direkte Verbrauchsmaterial-Belieferung von Endkunden gemäß Partner MPS
         Alliance-Einzelverträge)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Systeam GmbH, Industriestraße 8, 96250 Ebensfeld (Dropshipping)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Synaxon AG, Falkenstraße 31, 33758 Schloß Holte-Stukenbrock (eProcurement /
         Management von Beschaffungskonditionen)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         pcvisit Software AG, Manfred-von-Ardenne-Ring 20, 01099 Dresden
         (Fernwartungen)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Annegrit Schmiedeknecht, Einzelunternehmerin, Obervellmaer Str. 3, 34128
         Kassel (IT-Schulungen)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Wortmann AG (Leistungen im Bereich Informationstechnologie Produkt & Handel,
         Service & Dienstleistungen)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Microsoft Corp., Konrad-Zuse-Str.1, 85716 Unterschleißheim (Einsatz und
         Verwaltung von MS365)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         DHL Paket GmbH, Sträßchensweg 10, 53113 Bonn (Paketversand)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         UNITED PARCEL SERVICE DEUTSCHLAND S.à r.l. & Co. OHG, Görlitzer Straße 1,
         41460 Neuss (Paketversand)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         General Logistics Systems Germany GmbH & Co. OHG, GLS Germany-Straße 1 - 7,
         36286 Neuenstein (Paketversand)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         trans-o-flex Express GmbH & Co. KGaA, Hertzstraße 10, 69469 Weinheim
         (Paketversand)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         G Data CyberDefense AG, Königsallee 178, 44799 Bochum (Vertrieb von Waren
         und Dienstleistungen; Installationsunterstützung; Support; online
         Awareness-Trainings)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         Intos Electronic AG, Siemensstraße 11, 35394 Gießen (Verwaltung
         personenbezogener Daten für die Durchführung des Auftrags beziehungsweise
         zur Abwicklung der Geschäftsbeziehung)
     </li>
 
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         AEM Communication GmbH & Co. KG, Obergasse 40-42, 36304 Alsfeld (Wartung und
         Pflege Kundenmanagement; Bereitstellung SaaS f. Kundenmanagement; Support)
     </li>
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         ALL-INKL.COM - Neue Medien Münnich, Inhaber René Münnich, Hauptstraße 68B,
         02742 Friedersdorf (Web Hosting und Vertrieb von Hosting Verträgen)
     </li>
-    <li style="display: inline-flex; gap: 0.5rem; vertical-align: middle">
+    <li style="display: flex; gap: 0.5rem; align-items: baseline">
         <input type="checkbox" checked disabled />
         meetergo GmbH - Hansaring 61, 50670 Köln (Online Beratungstermine
         Verwaltung)
