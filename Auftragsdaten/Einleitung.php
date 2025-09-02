0d487fa9065cdf081617a3b37c1906f5bd7fd72b
jayzone91 <register@j4yz0n3.de>
2025-09-02 09:42:10 +0200

    Update Einleitung.php

--- Auftragsdaten/Einleitung.php
+++ Auftragsdaten/Einleitung.php
@@ -7,13 +7,13 @@ function Einleitung(string $Kundennummer, string $Firma, string $Adresse, string
     $Adresse = htmlspecialchars($Adresse);
     $Ort = htmlspecialchars($Ort);
 
-    return `<bookmark content="Einleitung" />
+    return '<bookmark content="Einleitung" />
     <h1>
       Datenschutzvereinbarung zur Auftragsverarbeitung gemäß Art. 28 DS-GVO
     </h1>
     <p>zwischen dem Verantwortlichen:</p>
     <p>
-    ` . "Kundennummer: $Kundennummer<br>$Firma<br>$Adresse<br>$Ort<br>" . `
+    ' . "Kundennummer: $Kundennummer<br>$Firma<br>$Adresse<br>$Ort<br>" . '
     (nachstehend Auftraggeber genannt)
     </p>
     <p>und dem Auftragsverarbeiter:</p>
@@ -22,5 +22,5 @@ function Einleitung(string $Kundennummer, string $Firma, string $Adresse, string
       Harleshäuser Str. 8<br />
       34130 Kassel<br />
       (nachstehend Auftragnehmer genannt)
-    </p>`;
+    </p>';
 }
