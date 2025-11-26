f2317fb914d1a04a736648658bfba5afe113e5dc
Johannes Kirchner <register@j4yz0n3.de>
2025-11-26 13:59:40 +0100

    show errors

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
-declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', TRUE);
 
+declare(strict_types=1);
 
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
